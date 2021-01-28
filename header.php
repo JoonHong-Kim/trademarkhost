4d79beecf1ee2e0c6193432d81640dd8d24f8a93
JOONHONG <danielkim30433@gmail.com>
2021-01-28 15:12:46 +0900

    Header 부분 + Icon

---
+++ header.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+
+<html prefix="og: http://ogp.me/ns#" lang="en">
+
+<head>
+    <meta name="google-site-verification" content="h37WWmUf9rgktjt6isApfIo9dyqPS0d47RzKVLSXsk4">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./static/site/style.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+    <script>
+        // Set to the same value as the web property used on the site
+        var gaProperty = 'UA-8236687-2';
+
+        // Disable tracking if the opt-out cookie exists.
+        var disableStr = 'ga-disable-' + gaProperty;
+        if (document.cookie.indexOf(disableStr + '=true') > -1) {
+            window[disableStr] = true;
+        }
+
+        // Opt-out function
+        function gaOptout() {
+            document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+            window[disableStr] = true;
+            $('.cnr-alert').removeClass('hidden');
+        }
+    </script>
+
+
+    <title>Trademarkhost: worldwide trademark registration - Trademarkhost</title>
+    <meta name="generator" content="Lumen CMS">
+    <meta name="description" content="Trademark registration: register a national or European Trademark. Trademarkhost is an online service for worldwide trademark registration and protection.">
+    <meta name="keywords" content="trademark registration, worldwide trademark protection, brand protection, logo protection ">
+    <meta property="og:title" content="Trademarkhost: worldwide trademark registration">
+    <meta property="og:description" content="Trademark registration: register a national or European Trademark. Trademarkhost is an online service for worldwide trademark registration and protection.">
+    <meta property="og:url" content="https://www.trademarkhost.com/">
+    <meta property="og:locale" content="en_GB">
+    <meta property="og:site_name" content="Trademarkhost">
+    <link rel="icon" href="./static/icons/favicon.ico">
+    <link rel="apple-touch-icon" href="./static/icons/apple-touch-icon-57x57.png" sizes="57x57">
+    <link rel="apple-touch-icon" href="./static/icons/apple-touch-icon-60x60.png" sizes="60x60">
+    <link rel="apple-touch-icon" href="./static/icons/apple-touch-icon-72x72.png" sizes="72x72">
+    <link rel="apple-touch-icon" href="./static/icons/apple-touch-icon-76x76.png" sizes="76x76">
+    <link rel="apple-touch-icon" href="./static/icons/apple-touch-icon-114x114.png" sizes="114x114">
+    <link rel="apple-touch-icon" href="./static/icons/apple-touch-icon-120x120.png" sizes="120x120">
+    <link rel="apple-touch-icon" href="./static/icons/apple-touch-icon-144x144.png" sizes="144x144">
+    <link rel="apple-touch-icon" href="./static/icons/apple-touch-icon-152x152.png" sizes="152x152">
+    <link rel="apple-touch-icon" href="./static/icons/apple-touch-icon-180x180.png" sizes="180x180">
+    <link rel="icon" href="./static/icons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" href="./static/icons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" href="./static/icons/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" href="./static/icons/android-chrome-192x192.png" sizes="192x192">
+</head>
+
+<body>
+    <div class="cnr-alert hidden">
+        <div class="alert alert-success" data-dismiss="alert" aria-label="Close">
+            <strong>Success!</strong> Google Analytics has been disabled.
+        </div>
+    </div>
+
+    <div id="wpr-header">
+        <div id="cnr-header" class="clearfix">
+            <a href="/" title="Trademarkhost" class="logo"><img src="./static/site/logo.png" alt="Trademarkhost" class="img-responsive"></a>
+            <button class="toggleMenu visible-xs"><span></span></button>
+
+            <ul class="hidden-xs">
+                <li><a href="/order-trademark-search" title="" class="search">Search</a></li>
+                <li><a href="/order-trademark-register" title="" class="register">Register</a></li>
+                <li><a href="/order-trademark-watch" title="" class="watch">Watch</a></li>
+            </ul>
+
+            <div class="callto hidden-xs">
+                <a href="tel:+821092478135" title="Call to +82 10 9247 8135"> +82 10 9247 8135</a>
+            </div>
+        </div>
+    </div>
+
+    <div id="wpr-menu">
+        <div id="cnr-menu">
+            <ul class="wpr_HoofdMenu">
+
+                <li><a href="/" title="Trademark registration"><span>Home</span></a></li>
+                <li><a href="/european-union-trademark" title="European Union Trademark"><span>European Union Trademark</span></a></li>
+                <li><a href="/about-trademarks" title=""><span>About trademarks</span></a></li>
+                <li><a href="/services" title=""><span>Services</span></a></li>
+                <li><a href="/worldwide-trademark-registration" title="Countries"><span>Countries</span></a></li>
+                <li><a href="/trademark-price-list" title=""><span>Fees</span></a></li>
+                <li><a href="/about-us" title=""><span>About us</span></a></li>
+                <li><a href="/contact" title="Contact"><span>Contact</span></a></li>
+            </ul>
+        </div>
+    </div>
