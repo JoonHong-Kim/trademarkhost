0aca92c653331bc6310d6d6aed039c6b6bbc34b1
JOONHONG <danielkim30433@gmail.com>
2021-01-28 15:18:29 +0900

    Main Page 완성

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
 <?php
 session_start();
-require_once 'header.php';
+require_once 'header.html';
+echo<<< _init
+<!DOCTYPE html>
+
+
+    
+    <div id="wpr-paginaAfbeelding">
+<div id="cnr-paginaAfbeelding" class="home clearfix">
+    <div class="slide">
+        <h1 class="hidden-xs">WORLDWIDE TRADEMARK REGISTRATION</h1>
+        <p class="hidden-xs">Our mission: making trademark protection worldwide accessible for everyone!<br>With over 120 Trademarkhost-agents around the world we strive to always offer our clients<br>a complete package of global trademark protection.</p>
+        
+        <p>Contact us for registering a national trademark in any country or a <a href="/european-union-trademark">European Union Trademark</a>.</p>
+        
+        <div class="youtube">
+            <img src="./static/site/YTtext.png" class="yttext img-responsive" alt="Start our video">
+            <a href="https://www.youtube.com/embed/rpGopVwM6eU" target="_blank" title="https://www.youtube.com/embed/rpGopVwM6eU"><img src="./static/site/YTmovie.jpg" class="img-responsive" alt="Start our video"></a>
+        </div>
+    </div>
+</div>
+</div>
+    
+    <div id="wpr-keuzen" class="clearfix">
+<div class="col-sm-4 keuze search">
+    <h3>Trademark search</h3>
+    <p>Before introducing a new trademark, the first concern is to find out whether it is available for use at all.</p>
+    <a href="/order-trademark-search" title="" class="ordernow">Order now<i class="fa fa-arrow-circle-right"></i></a>
+    <p><strong>Read more about</strong> <a href="/services" title="">Trademark search &raquo;</a></p>
+</div>
+<div class="col-sm-4 keuze register">
+    <h3>Trademark registration</h3>
+    <p>Trademarkhost offers trademark registration services in more than 120 countries in the world.</p>
+    <a href="/order-trademark-register" title="" class="ordernow">Order now<i class="fa fa-arrow-circle-right"></i></a>
+    <p><strong>Read more about</strong> <a href="/services" title="">Trademark registration &raquo;</a></p>
+</div>
+<div class="col-sm-4 keuze watch">
+    <h3>Trademark Watch</h3>
+    <p>Stop others from infringing upon your trademark; order a trademark watch now.</p>
+    <a href="/order-trademark-watch" title="" class="ordernow">Order now<i class="fa fa-arrow-circle-right"></i></a>
+    <p><strong>Read more about</strong> <a href="/services" title="">Trademark watching &raquo;</a></p>
+</div>
+</div>
+_init;
+require_once 'buttom.html';
